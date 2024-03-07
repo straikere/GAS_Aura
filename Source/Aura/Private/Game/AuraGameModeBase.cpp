b3d8270f1249f8b6a4c3bdd942a1ee49647b770a
STRAIKERE <straikere2403@gmail.com>
2024-03-07 16:31:04 +0200

    Add Interfaces, Setup Camera

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright notice
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright notice
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
