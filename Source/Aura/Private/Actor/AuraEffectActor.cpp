b5c8694ad9908e9850d81f5b464c30ab23a542ed
STRAIKERE <straikere2403@gmail.com>
2024-03-24 14:45:05 +0200

    Setup to Remove HandlerEffect on Actor

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -32,6 +32,16 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+	
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	
+}
+
 
 
 
